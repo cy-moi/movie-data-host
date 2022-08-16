e2f6baab55cc77de50cb95ff46d3fb7f5d31bd4b
zcy <congyao119@gmail.com>
2022-08-16 04:07:09 +0200

    try heroku

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
 import 'dotenv/config'
 import mongoose from 'mongoose';
-import express from 'express';
-import cors from 'cors';
-// import { ApolloServer } from 'apollo-server';
-import { ApolloServer } from 'apollo-server-express'
+import { ApolloServer } from 'apollo-server';
+
 import { typeDefs } from './typeDefs';
 import { resolvers } from './resolvers';
 import Movies from './dataSources/movies';
@@ -13,31 +11,31 @@ const main = async () => {
   await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 };
 
-const app = express();
-app.use(cors())
+// const connection = mongoose.connection;
+// // let collection;
 
-console.log("connection open")
+// connection.on('error', console.error.bind(console, 'connection error:'));
+// connection.on('open', function () {
+    console.log("connection open")
 
-main()
-    .then(async ()=> {
-        console.log('🎉 connected to database successfully')
-        const collection = await mongoose.connection.collection("watchlist");
-        // collection.find({}).toArray(function(err, result) {
-        //     console.log(result)
-        // })
-        const dataSources = () => ({
-            movies: new Movies(collection),
-        });
-        const server = new ApolloServer({ typeDefs, resolvers, dataSources })
-        await server.start()
-        server.applyMiddleware({ path: '/graphql', app})
-        app.listen(8080, ()=> {console.log(`🚀 Server ready `)})
-        // server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-        //     console.log(`🚀 Server ready at ${url}`);
-        // });
+    main()
+        .then(async ()=> {
+            console.log('🎉 connected to database successfully')
+            const collection = await mongoose.connection.collection("watchlist");
+            // collection.find({}).toArray(function(err, result) {
+            //     console.log(result)
+            // })
+            const dataSources = () => ({
+                movies: new Movies(collection),
+            });
+            const server = new ApolloServer({ typeDefs, resolvers, dataSources })
+        
+            server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+            console.log(`🚀 Server ready at ${url}`);
+            });
 
-    })
-    .catch(error => console.error(error));
+        })
+        .catch(error => console.error(error));
 
 
 
