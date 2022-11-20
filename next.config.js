f129a56a3608716deb9c76600af21048e5d5aae8
cy-moi <congyao119@gmail.com>
2022-11-20 08:34:13 +0100

    test aws server fetch

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/pages/index.html'
+      }
+    ]
+  }
 }
 
-module.exports = nextConfig
+module.exports = {
+  future: {
+    webpack5: true
+  },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    }
+    return config;
+  }
+}
--- pages/douban.js
+++ pages/douban.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import 'dotenv/config'
+// import ReactDom from 'react-dom/client';
 import * as Realm from "realm-web";
 import {
   ApolloClient,
-  // ApolloProvider,
+  ApolloProvider,
   HttpLink,
   InMemoryCache,
   gql
@@ -11,75 +13,76 @@ import {
 // Connect to your MongoDB Realm app
 const app = new Realm.App('doubanbackup-wpeti');
 // Gets a valid Realm user access token to authenticate requests
-// async function getValidAccessToken() {
-//   // Guarantee that there's a logged in user with a valid access token
-//   if (!app.currentUser) {
-//     // If no user is logged in, log in an anonymous user. The logged in user will have a valid
-//     // access token.
-//     await app.logIn(Realm.Credentials.anonymous());
-//   } else {
-//     // An already logged in user's access token might be stale. To guarantee that the token is
-//     // valid, we refresh the user's custom data which also refreshes their access token.
-//     await app.currentUser.refreshCustomData();
-//   }
-//   return app.currentUser.accessToken;
-// }
+async function getValidAccessToken() {
+  // Guarantee that there's a logged in user with a valid access token
+  if (!app.currentUser) {
+    // If no user is logged in, log in an anonymous user. The logged in user will have a valid
+    // access token.
+    await app.logIn(Realm.Credentials.anonymous());
+  } else {
+    // An already logged in user's access token might be stale. To guarantee that the token is
+    // valid, we refresh the user's custom data which also refreshes their access token.
+    await app.currentUser.refreshCustomData();
+  }
+  return app.currentUser.accessToken;
+}
 
-// const client = new ApolloClient({
-//   link: new HttpLink({
-//     uri: 'http://localhost:4000',
-//     fetch: async (uri, options) => {
-//       const accessToken = await getValidAccessToken();
-//       options.headers.Authorization = `Bearer ${accessToken}`;
-//       return fetch(uri, options);
-//     },
-//   }),
-//   cache: new InMemoryCache()
-// });
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: process.env.SERVER_URL,
+    fetch: async (uri, options) => {
+      const accessToken = await getValidAccessToken();
+      options.headers.Authorization = `Bearer ${accessToken}`;
+      return fetch(uri, options);
+    },
+  }),
+  cache: new InMemoryCache()
+});
 
-// const movieQuery = gql`
-//   query EaQuery {
-//     getMovies {
-//       _id
-//       ChineseTitle
-//       DoubanRating
-//     }
-//   }
-// `;
+const movieQuery = gql`
+  query EaQuery {
+    getMovies {
+      _id
+    }
+  }
+`;
 
-// export const getStaticProps = async () => {
-//   try {
-//     const { data: { getMovies } } = await client.query({query: movieQuery});
-//     console.log(getMovies.length)
-//     return { props: {getMovies} }
-//   } catch(err) {
-//     console.error('page error')
-//     throw (err)
-//   }
-// }
+export const getStaticProps = async () => {
+  try {
+    const { data: { getMovies } } = await client.query({ query: movieQuery });
+    console.log(getMovies.length)
+    return { props: { getMovies } }
+  } catch (err) {
+    console.error('page error')
+    throw (err)
+  }
+}
 
-export default function Douban() {
+export default function Douban({ getMovies }) {
   // console.log(watchlists)
   return (
-    <div>
-      <h2>My first Apollo app 🚀</h2>
-      {/* {getMovies ? getMovies.map(({ ChineseTitle, DoubanRating, _id }) => (
-        <div key={_id}>
-          <p>
-            {ChineseTitle}: {DoubanRating}
-          </p>
-        </div>
-      )) : null} */}
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+        {getMovies ? getMovies.map(({ _id }) => (
+          <div key={_id}>
+            <p>
+              {_id}
+            </p>
+          </div>
+        )) : null}
+      </div>
+    </ApolloProvider>
   );
 }
 
-// render(
-//   <ApolloProvider client={client}>
-//     <Douban />
-//   </ApolloProvider>,
-//   document.getElementById('root'),
-// );
+// if (typeof window !== 'undefined' && document.getElementById('root') !== 'undefined') {
+//   ReactDom.createRoot(document.getElementById('root')).render(
+//     <ApolloProvider client={client}>
+//       <Douban />
+//     </ApolloProvider>
+//   );
+// }
 
 
 
